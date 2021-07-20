0b9d9b7f007cd0f5dc685e347cd51c0aaea617b2
Erik S. Carlsten <ecarlste@gmail.com>
2021-07-20 12:37:17 -0600

    DX-444 Added a new test for negative addition

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -3,3 +3,7 @@ import { sum } from "../src/sum";
 test("adds 3 + 1 to equal 4", () => {
   expect(sum(3, 1)).toBe(4);
 });
+
+test("adding a negative number to a positive number should return correct result", () => {
+  expect(sum(-3, 1)).toBe(-2);
+});
